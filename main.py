1fff1bc82f9d776e9ce7c7df2f3b8c0abfbd7524
George Davidson <gd240673@gmail.com>
2023-02-06 22:10:22 -0500

    Update

--- main.py
+++ main.py
@@ -2,32 +2,26 @@ from pprint import pprint
 
 import requests
 
-# Your personal access token.
 token = "YOUR-PERSONAL-ACCESS-TOKEN"
 
-# Repository Settings
-name = "Test-Repository-1"
-description = "This repository was created with an API call."
+repo_name = "Test-Repository-1"
+repo_description = "This repository was created with an API call."
 include_all_branches = False
 private = False
 
-# Set the API endpoint URL.
 url = "https://api.github.com/user/repos"
 
-# Set the headers for authenticated requests.
 headers = {
     "Authorization": f"Bearer {token}"
 }
 
-# Set the request data.
 data = {
-    "name": name,
-    "description": description,
+    "name": repo_name,
+    "description": repo_description,
     "include_all_branches": include_all_branches,
     "private": private
 }
 
-# Send the PATCH request.
 response = requests.post(url, json=data, headers=headers)
 
 if response.status_code == 200:
